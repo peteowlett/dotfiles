02ff6a93b3f3b5e9c1ae1b44aa27fea1ec85a4a4
Peter Owlett <pete@deliveroo.co.uk>
2018-06-26 10:16:07 +0100

    first set of new gig changes

--- ipython.symlink/profile_default/startup/01_create_ssql_func.py
+++ ipython.symlink/profile_default/startup/01_create_ssql_func.py
@@ -1,23 +1,8 @@
-import os
-import pandas as pd
-import snowflake.connector
+from google.cloud import bigquery
 
-def get_ssql(query):
-    """
-    Execute a single SQL statement against snowflake, get
-    the results, parse to dataframe and return
-    """
-    cnx = snowflake.connector.connect(
-      user=os.environ['SNOWFLAKE_USERNAME'],
-      password=os.environ['SNOWFLAKE_PASSWORD'],
-      account=os.environ['SNOWFLAKE_HOST'],
-    )
+def get_bigquery(user_query):
+    client = bigquery.Client()
+    query_job = client.query(user_query)
+    results = query_job.result()
+    return(results)
 
-    cursor = cnx.cursor()
-    cursor.execute("USE WAREHOUSE RIDER_TRAINING;")
-    cursor.execute("USE DATABASE PRODUCTION;")
-    cursor.execute("USE SCHEMA RIDER_PLANNING;");
-
-    results = cursor.execute(query).fetchall()
-    df = pd.DataFrame(results, columns=[x[0] for x in cursor.description])
-    return df
