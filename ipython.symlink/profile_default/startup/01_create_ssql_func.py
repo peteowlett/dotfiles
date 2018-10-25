0244baa0d50738d2f338d1ca83f5655ab280c823
Peter Owlett <peteowlett@gmail.com>
2018-10-25 11:49:44 +0100

    Update after fixing zsh rc on work machine

--- ipython.symlink/profile_default/startup/01_create_ssql_func.py
+++ ipython.symlink/profile_default/startup/01_create_ssql_func.py
@@ -1,8 +1,10 @@
+import os
 from google.cloud import bigquery
 
 def get_bigquery(user_query):
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/pete/.gsutil/data-warehouse-942b03e59723.json'
     client = bigquery.Client()
     query_job = client.query(user_query)
     results = query_job.result()
-    return(results)
+    return results.to_dataframe()
 
